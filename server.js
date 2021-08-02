f3356b54817c08dedbe62cfdf7be0c0d2a0845b4
eliuabdiel <2018030333@upsin.edu.mx>
2021-08-02 01:24:48 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const dbUrl = 'mongodb+srv://abdiel:pet-project@pet-project.k1oxe.mongodb.net/pet-project?retryWrites=true&w=majority';
+
+const Review = mongoose.model('Review',{
+  user: String,
+  review: String,
+  track: String,
+  date: Date,
+})
+
+app.use(
+  cors({  
+    origin: 'http://localhost:3000',
+  })  
+);
+app.use(express.json());
+app.get('/hello', (req, res) => {
+  res.json("hello world");
+});
+
+app.post('/review',async (req, res) => {
+  try{
+    const review = new Review(req.body);
+    const savedMessage = await review.save();
+    res.sendStatus(200);
+  }catch(error){
+    res.sendStatus(500);
+  }
+
+});
+
+app.get('/reviews/:track', async (req, res) => {
+  const reviews = await Review.find({ track: req.params.track });
+  res.json(reviews);
+});
+
+mongoose.connect(
+  dbUrl,
+  {useNewUrlParser: true, useUnifiedTopology: true},
+  (error) => {
+    console.log('mongodb connection ', error);
+  }
+);
+
+app.listen(8080,()=>{
+  console.log("server started on port 8080")
+});
